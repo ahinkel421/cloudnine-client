4b544f01472ba261a257f6ba44f9c5adb7542beb
Adam Hinkel <ahinkel421@gmail.com>
2017-12-07 12:40:27 -0500

    config update

--- src/components/lounges.js
+++ src/components/lounges.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './navbar.css';
 import LoungeBox from './lounge-box';
 
+import {API_BASE_URL} from '../config';
+
 export default class Lounges extends React.Component {
 
   constructor(props){
@@ -12,7 +14,7 @@ export default class Lounges extends React.Component {
   componentDidMount(){
     var self = this;
 
-    fetch('http://localhost:8080/lounges').then(function(response) {
+    fetch(`${API_BASE_URL}/lounges`).then(function(response) {
       return response.json();
     })
     .then(function(data) {
